e04c8e35d6cc96242a0bb6ac04690e9e71a05464
cz <carloskalaykytedin@gmail.com>
2025-02-22 13:24:59 +0100

    3.19-3.21

--- models/person.js
+++ models/person.js
@@ -21,16 +21,28 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate : {
+      validator: function(value) {
+        return /\d{2,3}-\d{8}/.test(v);
+      },
+      message: 'Not a valid phone number'
+    }
+  },
   id: Number
 })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
+    //Cant delete or else the frontend wont be able to find the correct id and thats why it wouldnt work
+    //Since i am learning, for now ill leave the console log with the ids but in the future
+    //ill know they gotta be hidden
+    // delete returnedObject._id
     // delete returnedObject.id
-    delete returnedObject.__v
+    // delete returnedObject.__v
   }
 })
 
